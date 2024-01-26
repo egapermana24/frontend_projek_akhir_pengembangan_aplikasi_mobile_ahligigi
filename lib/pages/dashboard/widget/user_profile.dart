bfe3e80a2f84fbe41fe187196ad5f37c55df564e
Ega Permana <permanaega677@gmail.com>
2024-01-26 15:27:35 +0700

    mengubah halaman login dan halaman dashboard

--- lib/pages/dashboard/widget/user_profile.dart
+++ lib/pages/dashboard/widget/user_profile.dart
@@ -1,3 +1,4 @@
+import 'package:ahli_gigi/pages/navbar/navbar.dart';
 import 'package:ahli_gigi/settings/constants/warna_apps.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -6,79 +7,142 @@ import 'package:ahli_gigi/pages/profile/profile.dart';
 
 class UserProfile extends StatelessWidget {
   final User? user;
-
   final VoidCallback? onPressed;
 
   UserProfile({Key? key, required this.user, this.onPressed}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // fullName
     String fullName = user?.displayName ?? '';
+    String shortName =
+        fullName.isNotEmpty ? fullName.split(' ')[0] : 'Pengguna';
     String joinDate = user != null
         ? 'Bergabung sejak ${user?.metadata.creationTime != null ? DateFormat('dd MMM yyyy').format(user!.metadata.creationTime!.toLocal()) : ''}'
-        : '';
-    String email = user?.email ?? '';
-    return Row(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: InkWell(
-            onTap: onPressed,
-            child: Container(
-              decoration: BoxDecoration(
-                  border: Border.all(color: const Color.fromARGB(0, 0, 0, 0))),
-              child: Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.2),
-                          spreadRadius: 1,
-                          blurRadius: 5,
-                          offset: Offset(0, 2),
+        : 'Belum Bergabung';
+    String email = user?.email ?? 'Tidak ada email';
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      width: double.infinity, // Membuat container selebar layar
+      child: InkWell(
+        onTap: onPressed,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: const Color.fromARGB(0, 0, 0, 0)),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: CircleAvatar(
+                  radius: 32,
+                  backgroundColor: AppColors.primaryColor,
+                  child: CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.white,
+                    backgroundImage: user?.photoURL != null
+                        ? NetworkImage(user!.photoURL!)
+                        : AssetImage('assets/img/empty-user.png')
+                            as ImageProvider,
+                  ),
+                ),
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Halo, $shortName!',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.primaryText,
+                          ),
+                        ),
+                        Text(
+                          email,
+                          style: TextStyle(fontSize: 14, color: Colors.grey),
+                        ),
+                        Text(
+                          joinDate,
+                          style: TextStyle(fontSize: 10, color: Colors.grey),
                         ),
                       ],
                     ),
-                    child: CircleAvatar(
-                      radius: 33,
-                      backgroundColor:
-                          AppColors.primaryColor, // Warna latar belakang
-                      child: CircleAvatar(
-                        radius: 30,
-                        backgroundImage: NetworkImage(user?.photoURL ?? ''),
-                        backgroundColor:
-                            Colors.white, // Warna latar belakang gambar
-                      ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: AppColors.primaryColor),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          margin: EdgeInsets.only(bottom: 8.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10.0),
+                            child: Padding(
+                              padding: const EdgeInsets.all(
+                                  4.0), // Sesuaikan dengan ukuran padding yang diinginkan
+                              child: Image.asset(
+                                'assets/icons/bell.png',
+                                height: 20,
+                                width: 20,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: AppColors.primaryColor),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10.0),
+                            child: InkWell(
+                              onTap: () {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        NavBar(initialPageIndex: 3),
+                                  ),
+                                );
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.all(
+                                    4.0), // Sesuaikan dengan ukuran padding yang diinginkan
+                                child: Image.asset(
+                                  'assets/icons/ic_selected_category.png',
+                                  height: 20,
+                                  width: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                  SizedBox(width: 16),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        fullName,
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        email,
-                        style: TextStyle(fontSize: 14, color: Colors.grey),
-                      ),
-                      Text(
-                        joinDate,
-                        style: TextStyle(fontSize: 10, color: Colors.grey),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
-      ],
+      ),
     );
   }
 }
--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -1,5 +1,6 @@
 import 'package:ahli_gigi/pages/dashboard/dashboard.dart';
 import 'package:ahli_gigi/pages/navbar/navbar.dart';
+import 'package:ahli_gigi/settings/constants/warna_apps.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '/components/app_text_form_field.dart';
@@ -38,16 +39,18 @@ class _LoginState extends State<Login> {
           child: Column(
             children: [
               Container(
-                height: 240,
+                height: MediaQuery.of(context).size.height / 1.5,
                 width: double.infinity,
                 padding: const EdgeInsets.all(20),
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      Color.fromARGB(255, 63, 63, 63),
-                      Color.fromARGB(255, 90, 90, 90),
-                      Color.fromARGB(255, 66, 66, 66),
-                    ],
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/img/login_screen.jpg'),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(
+                      Colors.black
+                          .withOpacity(0.5), // Ganti nilai 0.5 sesuai kebutuhan
+                      BlendMode.darken,
+                    ),
                   ),
                 ),
                 child: Column(
@@ -55,7 +58,7 @@ class _LoginState extends State<Login> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      'Sign in to your\nAccount',
+                      'Selalu Jaga Kesehatan Gigimu\nBersama Acenk Dental',
                       style: TextStyle(
                           color: Colors.white,
                           fontSize: 20,
@@ -65,7 +68,7 @@ class _LoginState extends State<Login> {
                       height: 6,
                     ),
                     Text(
-                      'Sign in to your Account',
+                      'Silahkan Login Menggunakan Akunmu',
                       style: TextStyle(color: Colors.white),
                     ),
                   ],
@@ -75,104 +78,11 @@ class _LoginState extends State<Login> {
                 padding:
                     const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    AppTextFormField(
-                      labelText: 'Email',
-                      keyboardType: TextInputType.emailAddress,
-                      textInputAction: TextInputAction.next,
-                      onChanged: (value) {
-                        _formKey.currentState?.validate();
-                      },
-                      controller: emailController,
-                    ),
-                    AppTextFormField(
-                      labelText: 'Password',
-                      keyboardType: TextInputType.visiblePassword,
-                      textInputAction: TextInputAction.done,
-                      onChanged: (value) {
-                        _formKey.currentState?.validate();
-                      },
-                      controller: passwordController,
-                      obscureText: isObscure,
-                      suffixIcon: Padding(
-                        padding: const EdgeInsets.only(right: 15),
-                        child: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              isObscure = !isObscure;
-                            });
-                          },
-                          style: ButtonStyle(
-                            minimumSize: MaterialStateProperty.all(
-                              const Size(48, 48),
-                            ),
-                          ),
-                          icon: Icon(
-                            isObscure
-                                ? Icons.visibility_off_outlined
-                                : Icons.visibility_outlined,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Center(
-                      child: TextButton(
-                        onPressed: () {
-                          Navigator.pushReplacementNamed(context, 'forgot');
-                          // Tambahkan logika untuk tombol login di sini
-                        },
-                        style: Theme.of(context).textButtonTheme.style,
-                        child: Text(
-                          'Forgot Password?',
-                          style:
-                              Theme.of(context).textTheme.bodySmall?.copyWith(
-                                    color: Colors.blue,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                        ),
-                      ),
-                    ),
                     const SizedBox(
-                      height: 15,
-                    ),
-                    Center(
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    NavBar(initialPageIndex: 0),
-                              ));
-                          // Tambahkan logika untuk tombol login di sini
-                        },
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all(Color(0xFF505050)),
-                          foregroundColor:
-                              MaterialStateProperty.all(Colors.white),
-                          textStyle: MaterialStateProperty.all(TextStyle(
-                            fontSize: 13,
-                            fontWeight: FontWeight.bold,
-                          )),
-                          fixedSize: MaterialStateProperty.all(
-                              Size(500, 35)), // Atur ukuran di sini
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(
-                                  13.0), // Atur sudut di sini
-                            ),
-                          ),
-                        ),
-                        child: Text('Login'),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 30,
+                      height: 10,
                     ),
                     Row(
                       children: [
@@ -186,7 +96,7 @@ class _LoginState extends State<Login> {
                             horizontal: 20,
                           ),
                           child: Text(
-                            'Or login with',
+                            'Login with',
                             style: Theme.of(context)
                                 .textTheme
                                 .bodySmall
@@ -201,76 +111,143 @@ class _LoginState extends State<Login> {
                       ],
                     ),
                     const SizedBox(
-                      height: 30,
+                      height: 15,
                     ),
                     Row(
                       children: [
                         Expanded(
-                          child: OutlinedButton.icon(
-                            onPressed: () => AuthService.signInWithGoogle(context),
-                            style: Theme.of(context).outlinedButtonTheme.style,
-                            label: const Text(
-                              'Google',
-                              style: TextStyle(color: Colors.black),
-                            ),
-                            icon: FaIcon(
-                              FontAwesomeIcons.google,
-                              color: Colors.black,
+                          child: Material(
+                            elevation: 3, // Tinggi shadow
+                            borderRadius: BorderRadius.circular(15.0),
+                            child: Ink(
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: AppColors.primaryColor),
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                              child: InkResponse(
+                                onTap: () =>
+                                    AuthService.signInWithGoogle(context),
+                                child: Container(
+                                  padding: const EdgeInsets.all(12),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Image.asset(
+                                        'assets/icons/google.png',
+                                        height: 20,
+                                        width: 20,
+                                      ),
+                                      const SizedBox(width: 8),
+                                      const Text('Google'),
+                                    ],
+                                  ),
+                                ),
+                              ),
                             ),
                           ),
                         ),
-                        // const SizedBox(
-                        //   width: 20,
-                        // ),
-                        // Expanded(
-                        //   child: OutlinedButton.icon(
-                        //     onPressed: () {},
-                        //     style: Theme.of(context).outlinedButtonTheme.style,
-                        //     label: const Text(
-                        //       'Facebook',
-                        //       style: TextStyle(color: Colors.black),
-                        //     ),
-                        //     icon: FaIcon(
-                        //       FontAwesomeIcons.facebook,
-                        //       color: Colors.blueAccent,
-                        //     ), // Gantilah dengan ikon yang sesuai jika diperlukan
-                        //   ),
-                        // ),
                       ],
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 3, horizontal: 25),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Don't have an account?",
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodySmall
-                          ?.copyWith(color: Colors.black),
                     ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pushReplacementNamed(context, 'register');
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            color: Colors.grey.shade200,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                          ),
+                          child: Text(
+                            'This Is Us',
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall
+                                ?.copyWith(color: Colors.black),
+                          ),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            color: Colors.grey.shade200,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => NavBar(initialPageIndex: 0),
+                          ),
+                        );
                         // Tambahkan logika untuk tombol login di sini
                       },
-                      style: Theme.of(context).textButtonTheme.style,
-                      child: Text(
-                        'Register Here',
-                        style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                              color: Colors.blue,
-                              fontWeight: FontWeight.bold,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: AppColors.primaryColor,
+                          borderRadius: BorderRadius.circular(13.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.2),
+                              spreadRadius: 1,
+                              blurRadius: 5,
+                              offset: Offset(0, 2),
                             ),
+                          ],
+                        ),
+                        padding: EdgeInsets.all(12),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(50),
+                          child: Image.asset(
+                            'assets/img/logo-apps.png',
+                            height: 30,
+                            width: 30,
+                          ),
+                        ),
                       ),
                     ),
                   ],
                 ),
               ),
+              // Padding(
+              //   padding:
+              //       const EdgeInsets.symmetric(vertical: 3, horizontal: 25),
+              //   child: Row(
+              //     mainAxisAlignment: MainAxisAlignment.center,
+              //     children: [
+              //       Text(
+              //         "Don't have an account?",
+              //         style: Theme.of(context)
+              //             .textTheme
+              //             .bodySmall
+              //             ?.copyWith(color: Colors.black),
+              //       ),
+              //       TextButton(
+              //         onPressed: () {
+              //           Navigator.pushReplacementNamed(context, 'register');
+              //           // Tambahkan logika untuk tombol login di sini
+              //         },
+              //         style: Theme.of(context).textButtonTheme.style,
+              //         child: Text(
+              //           'Register Here',
+              //           style: Theme.of(context).textTheme.bodySmall?.copyWith(
+              //                 color: Colors.blue,
+              //                 fontWeight: FontWeight.bold,
+              //               ),
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // ),
             ],
           ),
         ),
